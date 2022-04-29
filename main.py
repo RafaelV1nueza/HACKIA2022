20a818197f11986a70f88b36db74100074295fc1
RafaelV1nueza <a01706351@tec.mx>
2022-04-29 15:58:40 -0500

    main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@
 #main
 if __name__ == '__main__':
     print("Hola Mundo!")
+    #define user parameters
+    age = input("Edad: ")
+
+
+    #calculo con usos
+
+    #variables 
+    heart_rate = 1
+    insuline_levels = 2
+    b_oxy = 3
+    b_pressureS = 4
+    b_pressureD = 5
